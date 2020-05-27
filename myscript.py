9ae1101fce5e28e71e53f1ad16262ebb88c71370
m2chanchal <54202986+m2chanchal@users.noreply.github.com>
2020-05-27 22:29:36 +0530

    Create myscript.py

---
+++ myscript.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import os
+import boto3
+
+#here = os.path.dirname(__file__)
+#temp_body = os.path.join(here, 'dockerimages')
+temp_body = 'example.yml'
+
+cfn = boto3.client('cloudformation')
+
+
+def launch_stack():
+    stackname = 'example_deploy.yaml'
+	capabilities = ['CAPABILITY_IAM', 'CAPABILITY_AUTO_EXPAND']
+	stackdata = cfn.create_stack(
+      StackName=stackname,
+      DisableRollback=True,
+      TemplateBody=temp_body,
+      Capabilities=capabilities)
+	  
+if __name__=="__main__":
+    launch_stack()
+        
